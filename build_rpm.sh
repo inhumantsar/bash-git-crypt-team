842a86c85b3fcff2d1d7b80aa052850401366bc1
Shaun Martin <shaun@samsite.ca>
2017-12-05 11:23:28 -0500

    adding build scripts

---
+++ build_rpm.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+SCRIPT=$(readlink -f "$0")
+DIR=$(dirname "$SCRIPT")
+
+PKG_NAME='git-crypt-team'
+SUMMARY="Centralized key management and rekeying for teams using git-crypt."
+URL="https://github.com/inhumantsar/bash-git-crypt-team"
+MAINTAINER="Shaun Martin <shaun@samsite.ca>"
+
+fpm -s dir -t rpm -f -C $DIR \
+  -n $PKG_NAME --prefix /usr/bin \
+  -v "$(cat VERSION)" -a noarch \
+  -m "${MAINTAINER}" --vendor "${MAINTAINER}" -d git -d gpg -d jq \
+  --description "$(cat README.md)" \
+  --rpm-summary "${SUMMARY}" \
+  --url "$URL" --license BSD git-crypt-team
